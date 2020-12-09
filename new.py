a1ae00dfcf76ded19120b9fbc7d9dfe79335d951
AhmetInal <45859216+AhmetInal@users.noreply.github.com>
2020-12-09 16:01:55 +0300

    Add files via upload
    Py Baslangıç komutları

---
+++ new.py
@@ -0,0 +1,25 @@
+x = 'ali,veli,selami'
+
+y='mahmut abi buraya bak'
+
+print(x)
+
+print(len(x))
+
+print(x.split(','))
+ 
+print(y[0:])
+
+print(x.title())
+print(x.upper())
+print(x.lower())
+
+print(x.count(','))
+print(x.find('a'))
+print(x.replace(',',':'))
+print(x)
+
+print(dir(x))
+m=(dir(x))
+
+print(help(list))
